e3df1bfaf7ba6fa98da3f9a5094cc704dbcfa3b8
Tomo <ydhu@robosense.cn>
2019-10-31 17:46:57 +0800

    地图

--- demo/raphaelDemo/draw_method.js
+++ demo/raphaelDemo/draw_method.js
@@ -4,15 +4,27 @@ let attr = {
     "stroke-width": 1,
     "stroke-linejoin": "round"
 };
-function drawPath(R){
-    // let url="./path.json"
-    // let request =new XMLHttpRequest()
-    // request.open("get",url)
-    // request.send(null)
-    // request.onload=(res)=>{
-    // }
-    return {
-        name:"河南",
-        path:R.path(HE_NAN).attr(attr)
+function drawPath(R,pathData){
+    return R.path(pathData.path).attr(attr)
+}
+
+let addTextAndEvent=(path,name,r,color)=>{
+    let text=null
+    // path.color=Raphael.getColor(0.9);
+    let centerX=path.getBBox().x+(path.getBBox().width/2)
+    let centerY=path.getBBox().y+(path.getBBox().height/2)
+    text=r.text(centerX,centerY,name).attr({fill:"#000",fontSize:"30px",cursor:"pointer"})
+    text.node.onclick=(e)=>{
+        window.location.href="https://map.baidu.com/search?querytype=s&da_src=shareurl&wd="+name
     }
+    path[0].onmouseover=()=>{
+        path.animate({fill:color,stroke:'#eee'},500)
+        r.safari()
+    }
+    path[0].onmouseout=()=>{
+        path.animate({fill:"#97d6f5",stroke:'#eee'},500)
+        // text.toFront()
+        r.safari()
+    }
+    return text
 }
